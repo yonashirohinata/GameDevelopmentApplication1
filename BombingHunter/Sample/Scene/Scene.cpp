e92215deefd006b1c56481f1573ffe6464eb81c5
yonashirohinata <138412434+yonashirohinata@users.noreply.github.com>
2024-04-12 11:51:44 +0900

    0202サンプルプログラム書き写し済

---
+++ BombingHunter/Sample/Scene/Scene.cpp
@@ -0,0 +1,63 @@
+#include "Scene.h"
+
+#include "../Objects/Player/Player.h"
+
+//コンストラクタ
+Scene::Scene() : objects()
+{
+
+}
+
+//デストラクタ
+Scene::~Scene()
+{
+	//忘れ防止
+	Finalize();
+}
+
+//初期化処理
+void Scene::Initialize()
+{
+	//プレイヤーを生成する
+	CreateObject<Player>(Vector2D(320.0f, 240.0f));
+}
+
+//更新処理
+void Scene::Update()
+{
+	//シーンに存在するオブジェクトの更新処理
+	for (GameObject* obj : objects)
+	{
+		obj->Update();
+	}
+}
+
+//描画処理
+void Scene::Draw() const
+{
+	//シーンに存在するオブジェクトの更新処理
+	for (GameObject* obj : objects)
+	{
+		obj->Draw();
+	}
+}
+
+//終了時処理
+void Scene::Finalize()
+{
+	//動的配列が空なら処理を終了する
+	if (objects.empty())
+	{
+		return;
+	}
+
+	//各オブジェクトを削除する
+	for (GameObject* obj : objects)
+	{
+		obj->Finalize();
+		delete obj;
+	}
+
+	//動的配列の開放
+	objects.clear();
+}
